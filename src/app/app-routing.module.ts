ed48c68d03174e5d48e1a50c46f747b9b27aac1d
mchaitanya <chaitanyaaa@gmail.com>
2018-11-08 21:27:00 -0600

    [cross-ctrl-vldn] Create cmp for cross control validation demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { DemosComponent } from './demos/demos.component';
+import { CrossCtrlVldnComponent } from './demos/cross-ctrl-vldn/cross-ctrl-vldn.component';
 
 export const routedCmps = [
     HomeComponent, 
-    DemosComponent
+    DemosComponent, 
+    CrossCtrlVldnComponent
 ];
 
 const routes: Routes = [
-    { path: 'demos', component: DemosComponent }, 
+    { 
+        path: 'demos', 
+        component: DemosComponent, 
+        children: [
+            { path: 'cross-ctrl-vldn', component: CrossCtrlVldnComponent }, 
+            { path: '', redirectTo: '/home', pathMatch: 'full'} 
+        ]
+    }, 
     { path: 'home', component: HomeComponent }, 
     { path: '', redirectTo: '/home', pathMatch: 'full' }, 
     { path: '**', redirectTo: '/home' }
---
+++ src/app/demos/cross-ctrl-vldn/cross-ctrl-vldn.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'demo-cross-ctrl-vldn', 
+    templateUrl: './cross-ctrl-vldn.component.html'
+})
+export class CrossCtrlVldnComponent { }
