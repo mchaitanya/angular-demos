d5c7027e828465be76c0d223b27907837776dd35
mchaitanya <chaitanyaaa@gmail.com>
2018-11-08 21:53:23 -0600

    [cross-ctrl-vldn] Create backing reactive form & hook it up to the tmpl

--- src/app/demos/cross-ctrl-vldn/cross-ctrl-vldn.component.ts
+++ src/app/demos/cross-ctrl-vldn/cross-ctrl-vldn.component.ts
@@ -1,7 +1,19 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'demo-cross-ctrl-vldn', 
     templateUrl: './cross-ctrl-vldn.component.html'
 })
-export class CrossCtrlVldnComponent { }
+export class CrossCtrlVldnComponent { 
+    form: FormGroup;
+
+    constructor() {
+        this.form = new FormGroup({
+            'curPwd': new FormControl(''), 
+            'newPwd': new FormControl(''), 
+            'confirm': new FormControl('')
+        });
+    }
+    
+}
