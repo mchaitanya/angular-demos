235a272f24967ad30b517c7a15c3e229896e0b9a
mchaitanya <chaitanyaaa@gmail.com>
2020-10-07 19:50:07 -0500

    Inject the ElementRef & Renderer into the directive
    Angular can inject the host element into the directive.
    The `Renderer` will help us manipulate the DOM.
    We now also log the `nodeName` of the host. At this point the logs should say:
    'sticky directive attached to H2' and
    'sticky directive attached to NAV'

--- src/app/demos/sticky-demo/sticky.directive.ts
+++ src/app/demos/sticky-demo/sticky.directive.ts
@@ -1,12 +1,17 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Directive({
     selector: '[sticky]'
 })
 export class StickyDirective implements OnInit {
+
+    constructor(private el: ElementRef, private renderer: Renderer2) {
+        //
+    }
     
     ngOnInit() {
-        console.log('sticky directive attached');
+        const host = <HTMLElement> this.el.nativeElement;
+        console.log('sticky directive attached to ' + host.nodeName);
     }
 
 }
