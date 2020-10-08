c0b471c4926bee066ba727042e7a5dbfb9dcb66e
mchaitanya <chaitanyaaa@gmail.com>
2020-10-08 15:54:09 -0500

    Manage a stream of modal definitions in the service
    The `ModalDfn` interface packages the various parts of a modal - the title, body & footer template.
    AppComponent will show/hide the modal based on values emitted in this stream.
    Other components can push modalDfns into the stream with the service's openModal & closeModal methods.

--- src/app/demos/reusable-modals/modal.service.ts
+++ src/app/demos/reusable-modals/modal.service.ts
@@ -1,6 +1,24 @@
-import { Injectable } from "@angular/core";
+import { Injectable, TemplateRef } from "@angular/core";
+
+import { Subject } from 'rxjs';
+
+export interface ModalDfn {
+    title: string;
+    body: TemplateRef<any>;
+    footer?: TemplateRef<any>;
+}
 
 @Injectable()
 export class ModalService {
-    //
+    private _modalDfn$ = new Subject<ModalDfn>();
+    public modalDfn$ = this._modalDfn$.asObservable();
+
+    openModal(modal: ModalDfn) {
+        this._modalDfn$.next(modal);
+    }
+
+    closeModal() {
+        this._modalDfn$.next(null);
+    }
+
 }
