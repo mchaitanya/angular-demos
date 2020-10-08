29b9401b60a0f26f7f257ec6bb43532551d97101
mchaitanya <chaitanyaaa@gmail.com>
2020-10-07 20:05:36 -0500

    Create a stream of scroll events
    We access the Window object & then use the `fromEvent` RxJS function to create the stream.
    We subscribe to the stream OnInit to get a chance to react to the events.
    Make sure to unsubscribe OnDestroy.

--- src/app/demos/sticky-demo/sticky.directive.ts
+++ src/app/demos/sticky-demo/sticky.directive.ts
@@ -1,10 +1,13 @@
-import { Directive, ElementRef, Renderer2, Inject, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Inject, OnInit, OnDestroy } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+
+import { fromEvent, Subscription } from 'rxjs';
 
 @Directive({
     selector: '[sticky]'
 })
-export class StickyDirective implements OnInit {
+export class StickyDirective implements OnInit, OnDestroy {
+    private sub: Subscription;
 
     constructor(private el: ElementRef, private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {
         //
@@ -13,6 +16,20 @@ export class StickyDirective implements OnInit {
     ngOnInit() {
         const host = <HTMLElement> this.el.nativeElement;
         console.log('sticky directive attached to ' + host.nodeName);
+
+        if (this.document) {
+            const window: Window = this.document.defaultView;
+            this.sub
+             = fromEvent(window, 'scroll')
+                .subscribe((ev: Event) => console.log(ev));
+        }
+
+    }
+
+    ngOnDestroy() {
+        if (this.sub) {
+            this.sub.unsubscribe();
+        }
     }
 
 }
