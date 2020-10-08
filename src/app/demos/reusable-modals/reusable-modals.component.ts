e2fdade0e90d31e53d5b3fd8de74b9505fee0a12
mchaitanya <chaitanyaaa@gmail.com>
2020-10-08 16:35:25 -0500

    Display a warning modal from the demo page

--- src/app/demos/reusable-modals/reusable-modals.component.ts
+++ src/app/demos/reusable-modals/reusable-modals.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+
+import { ModalService } from './modal.service';
 
 @Component({
     selector: 'app-reusable-modals', 
     templateUrl: './reusable-modals.component.html'
 })
 export class ReusableModalsComponent {
-    //
+    constructor(private modalService: ModalService) {
+        //
+    }
+
+    displayWarning(warningTmpl: TemplateRef<any>) {
+        this.modalService.openModal({
+            title: 'Warning', 
+            body: warningTmpl
+        });
+    }
+
 }
