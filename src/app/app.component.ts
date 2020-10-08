e177a42d02474a6341a8185f0611fa4a6e9f09c0
mchaitanya <chaitanyaaa@gmail.com>
2020-10-08 16:16:42 -0500

    React to the modalDfn$ stream in the AppComponent
    - Now we ngIf the modal in & out. We don't control it with the `display` style property.
    - If the stream emits a null, the modal is removed from the view
    - If the stream emits an ModalDfn object, then we take the body & footer template refs
    and instantiate them in the template with the *ngTemplateOutlet directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ModalDfn, ModalService } from './demos/reusable-modals/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent { 
-  isModalOpen = false;
+export class AppComponent implements OnInit, OnDestroy { 
+  modalDfn: ModalDfn;
+  sub: Subscription;
 
-  openModal() {
-    this.isModalOpen = true;
+  constructor(private modalService: ModalService) {
+    //
+  }
+
+  ngOnInit() {
+    this.sub
+     = this.modalService.modalDfn$
+        .subscribe((modalDfn: ModalDfn) => this.modalDfn = modalDfn);
+  }
+
+  openModal(title: string, modalBody: TemplateRef<any>, modalFooter: TemplateRef<any>) {
+    this.modalService.openModal({
+      title: title, 
+      body: modalBody, 
+      footer: modalFooter
+    });
   }
 
   closeModal() {
-    this.isModalOpen = false;
+    this.modalService.closeModal();
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
 }
